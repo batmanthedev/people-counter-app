1fe1d7839f38467989c07edfb0c7dcffa3b0d80f
batmanthedev <132001340+batmanthedev@users.noreply.github.com>
2023-04-28 10:52:11 +0200

    Add files via upload

---
+++ people-counter-app/js/index.js
@@ -0,0 +1,10 @@
+
+let count = 0;
+
+function increment()
+{
+    count++;
+    document.getElementById("count").innerText = count; // Gets an element specified by ID and sets the text of that element
+}
+
+
