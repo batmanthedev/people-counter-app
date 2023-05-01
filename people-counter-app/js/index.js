6f31fbf6eca7a21bd4cdc190ea533b0249fc63ef
batmanthedev <batmanthedev@gmail.com>
2023-05-01 22:11:10 +0200

    completed

--- people-counter-app/js/index.js
+++ people-counter-app/js/index.js
@@ -1,5 +1,6 @@
 
 let count = 0;
+let total = 0;
 
 function increment()
 {
@@ -7,4 +8,16 @@ function increment()
     document.getElementById("count").innerText = count; // Gets an element specified by ID and sets the text of that element
 }
 
+function save()
+{
+    total += count;
+    document.getElementById("save-history").innerText += " " + count;
+    count = 0;
+    document.getElementById("count").innerText = count;
+}
+
+function updateTotal()
+{
+    document.getElementById("total").innerText = "Total: " + total;
+}
 
